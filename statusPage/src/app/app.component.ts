5238d46384a4c18b2328044b9d6acf5058d9ba2f
Legend <support@legendeffects.co.uk>
2018-08-13 20:59:08 +0100

    Added placeholders

--- statusPage/src/app/app.component.ts
+++ statusPage/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'statusPage';
+    title = 'statusPage';
+    pageID = '6WE9VI3oN';
+    headerStyle = 'linear-gradient(323deg, #36D1DC 5%, #5B86E5)';
 }
