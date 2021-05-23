8fd616de4107dc80b7c364ffde8bead2e1973142
Legend <evaninho94@gmail.com>
2021-05-24 00:16:19 +0100

    refactor: remove old next frontend

--- packages/next/StatusifyContext.ts
+++
@@ -1,6 +0,0 @@
-import Statusify from '@statusify/core'
-import { createContext } from 'react'
-
-const StatusifyContext = createContext<Statusify>(null);
-
-export default StatusifyContext;
--- packages/next/src/LaminarOptions.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-import { LaminarOptions as LaminarOptionsType } from "./Laminar";
-
-export const LaminarOptions = createContext<LaminarOptionsType>(null);
-
-export default LaminarOptions
