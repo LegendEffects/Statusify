27c9504c794e46412f3b6d0b8ce059c7548f22ad
Legend <evaninho94@gmail.com>
2021-05-24 02:46:57 +0100

    feat(react): add related incidents to tooltip

--- packages/react/src/app/utils/i18n.ts
+++ packages/react/src/app/utils/i18n.ts
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
 
@@ -8,6 +9,15 @@ i18n.use(initReactI18next)
     },
     lng: 'en',
     fallbackLng: 'en',
+    interpolation: {
+      format: (value, format, lng) => {
+        if(value instanceof Date) {
+          return dayjs(value).format(format);
+        }
+
+        return value;
+      }
+    }
   })
 
   export default i18n;
